e7f52f38fd2243de9fe3ceec0d6a4c376b612e06
PV <prince369vishal@gmail.com>
2025-08-03 23:59:46 +0530

    StarPattern_04

--- StarPattern_04.java
+++ StarPattern_04.java
@@ -1,5 +1,7 @@
 public class StarPattern_04 {
   public static void main(String[] args){
+
+    //Use another way
     for(int i=1; i<=5; i++){
       for(int j=1;j<=i;j++){
         System.out.print("  ");
